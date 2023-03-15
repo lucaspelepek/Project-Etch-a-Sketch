740f49cabe705be5c2d9fe3a70767cf2f0d813ab
lucaspelepek <lucaspelepek@gmail.com>
2023-03-14 22:35:12 -0300

    Create and clean divs

--- javascript.js
+++ javascript.js
@@ -1,10 +1,30 @@
-document.addEventListener('DOMContentLoaded', createDivs);
+document.addEventListener('DOMContentLoaded', createDivs(16));
 
-function createDivs() {
+const buttomDiv = document.getElementById('buttomDiv').addEventListener('click', quantidadeDivsCriar);
+
+function quantidadeDivsCriar() {
+    let quantidade = prompt('how many squares you want?');
+    if (quantidade > 100) return alert("less than 100 please!");
+
+    createDivs(quantidade);
+}
+
+function cleanPreviousDivs() {
+        const divs = document.querySelectorAll('div');
+        divs.forEach(div => {
+          div.style.backgroundColor = 'white';
+        });
+      }
+      
+
+function createDivs(number) {
+
+    number *= number;
+    cleanPreviousDivs();
+
     const container = document.getElementById('container');
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < number; i++) {
         let div = document.createElement('div');
-        div.textContent = 'this is a test';
         container.appendChild(div);
     }
     roverDiv();
@@ -12,16 +32,16 @@ function createDivs() {
 
 function roverDiv() {
     const divElements = document.querySelectorAll('#container > div');
-    
-    divElements.forEach(function(divElement) {
-      divElement.addEventListener('mouseenter', function(event) {
-        event.target.style.backgroundColor = 'red';
-        // Add your code to perform an action when the mouse enters the div element
-      });
-      
-      divElement.addEventListener('mouseleave', function(event) {
-        //event.target.style.backgroundColor = '#ddd';
-        // Add your code to perform an action when the mouse leaves the div element
-      });
+
+    divElements.forEach(function (divElement) {
+        divElement.addEventListener('mouseenter', function (event) {
+            event.target.style.backgroundColor = 'red';
+            // Add your code to perform an action when the mouse enters the div element
+        });
+
+        divElement.addEventListener('mouseleave', function (event) {
+            //event.target.style.backgroundColor = '#ddd';
+            // Add your code to perform an action when the mouse leaves the div element
+        });
     });
 }
